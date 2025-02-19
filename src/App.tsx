658e99bca298d6941cf32824963115a68b8fb26f
Steven Pfeffer <stevenpfeffer6@gmail.com>
2025-02-19 19:39:01 +0100

    init setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    /* PLACE YOUR CODE HERE */
+    <div className="text-xl font-bold">
+      Programming is like sex: one mistake and you have to support it for the rest of your life.
+    </div>
+  );
+}
+
+export default App;
